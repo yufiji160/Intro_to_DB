596e11ae161f11dbb488a0ffcea9fc043d457ab9
yufiji160 <isaackkiobo0@gmail.com>
2025-10-15 11:09:23 +0300

    Add Populating Your Database with Your More Data

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,10 @@
 USE alx_book_store;
 
+DELETE FROM customer;
+
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+    (1, 'John Doe', 'jdoe@sandtech.com', '123 Happiness  Ave.'),
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
